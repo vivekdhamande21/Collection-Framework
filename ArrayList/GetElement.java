b4e3d9c60459b6074f291ab8adbed84257386cff
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-10-11 01:40:10 +0000

    Get element

---
+++ ArrayList/GetElement.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+class GetElement{
+    public static void main(String[] args){
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+        al.add(5);
+        System.out.println(al.get(2));
+    }
+}
